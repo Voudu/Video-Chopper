02b8726a29be0bde5c23d5edfbc36e5ab501766c
Voudu <alexescatel9@gmail.com>
2023-11-14 16:07:23 -0600

    add working project files

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Video-Chopper
-a python tool using moviepy that can creates any number of clips of any desired length from an .mp4 target
+# Video Chopper
+Creates clips of an .mp4 video using moviepy. Creates any number of clips of any duration from a .mp4 video. Does not include
+audio in the clips but could easily be made to.
+
+## Installation and Usage
+Create a virtual environment and install dependencies using the following commands:
+
+### Installation:
+```
+python -m videnv venv
+pip install -r requirements.txt
+videnv\Scripts\Activate.ps1 (depends on OS)
+```
+
+### Usage:
+```
+py video-chopper.py <video_path> <how_many_clips> <duration (sec)>
+```
+Note: the output will be in the root directory and create a folder called 'segments'
---
+++ chopper.py
@@ -0,0 +1,57 @@
+from moviepy.video.io.VideoFileClip import VideoFileClip
+import os
+
+def generate_clips(video_path, how_many_clips, duration, output_path):
+
+    if not os.path.exists(video_path):
+        print("File does not exist")
+        return
+
+    if not os.path.exists(output_path):
+        try:
+            os.mkdir(output_path)
+        except Exception as e:
+            print(f"failed to create a new directory for clips: {e}")
+            return
+
+    clipName = "clip_"
+
+    start = 0
+    inc = duration    # seconds per clip
+
+    trueOutput = output_path+"segments/"+clipName
+
+    videoFile = VideoFileClip(video_path)
+
+    for i in range(how_many_clips):
+        output = trueOutput+str(i)+".mp4"   # just naming the file
+
+        try: 
+            clip = videoFile.subclip(start, start+inc).without_audio()
+            clip.write_videofile(output, audio=False)
+        except Exception as e:
+            print(f"an error occurred during clipping: {e}")
+            return
+        finally:
+            clip.close()
+        # increment the start of the next clip
+        start += inc
+
+    videoFile.close()
+
+def main():
+    import sys
+    print(sys.executable)
+    if len(sys.argv) != 4:
+        print("Usage: python3 chopper.py <video_path> <how_many_clips> <duration (sec)>")
+        return
+
+    video_path = sys.argv[1]
+    how_many_clips = int(sys.argv[2])
+    clip_duration = int(sys.argv[3])
+    output_path = "./"
+
+    generate_clips(video_path, how_many_clips, clip_duration, output_path)
+
+if __name__ == "__main__":
+    main()
